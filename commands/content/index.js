57932d773ffafb2b31037bec2863cf7d5dcbb145
David <da70@nyu.edu>
2016-03-12 00:51:21 -0500

    content/index.js: expand "content" command a little bit

--- commands/content/index.js
+++ commands/content/index.js
@@ -3,5 +3,14 @@
 module.exports = function( vorpal ){
     vorpal.log( `Loaded ${ __filename }.` );
 
-    vorpal.command( 'content', 'Manage exploded EPUB content.' );
+    vorpal.command( 'content' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Manage exploded EPUB content.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log( args );
+
+                callback();
+            }
+        );
 };
