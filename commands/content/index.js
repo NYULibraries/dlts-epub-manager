0d7946610e0145986b33e67a43b12cdb533dda08
David <da70@nyu.edu>
2016-03-12 20:47:24 -0500

    content: add shells for delete, delete all, and full-replace

--- commands/content/index.js
+++ commands/content/index.js
@@ -24,4 +24,37 @@ module.exports = function( vorpal ){
                 callback();
             }
         );
+
+    vorpal.command( 'content delete' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Delete EPUB content from deployed collection.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'content delete all' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Delete all EPUB content from deployed collection.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'content full-replace' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Replace EPUB content of entire deployed collection.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log( args );
+
+                callback();
+            }
+        );
 };
