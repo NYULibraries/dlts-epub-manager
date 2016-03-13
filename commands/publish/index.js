b6368c319ad0496aa9443a289626adf3b0f869ac
David <da70@nyu.edu>
2016-03-12 21:25:26 -0500

    publish/: initial commit with basic interface shell

---
+++ commands/publish/index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+module.exports = function( vorpal ){
+    vorpal.log( `Loaded ${ __filename }.` );
+
+    vorpal.command( 'publish' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Publish EPUBs.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'publish add' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Add EPUBs.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'publish delete' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Delete EPUBs.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'publish delete all' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Delete all EPUBs.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'publish full-replace' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Replace all EPUBs.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+};
