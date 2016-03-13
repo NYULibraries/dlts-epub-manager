8b5db0ecc278259757cfbe84deff550dcdf8b073
David <da70@nyu.edu>
2016-03-12 20:42:55 -0500

    load/index.js: write load command shell

--- commands/load/index.js
+++ commands/load/index.js
@@ -2,4 +2,14 @@
 
 module.exports = function( vorpal ){
     vorpal.log( `Loaded ${ __filename }.` );
+
+    vorpal.command( 'load <configuration>' )
+        .description( 'Read in configuration file and load resources.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log( args );
+
+                callback();
+            }
+        );
 };
