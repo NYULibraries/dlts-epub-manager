3e3d722fc5db151e051a0908d5acb2f5acebd1c9
David <da70@nyu.edu>
2016-03-12 21:23:12 -0500

    handles/index.js: basic interface shell

--- commands/handles/index.js
+++ commands/handles/index.js
@@ -2,4 +2,63 @@
 
 module.exports = function( vorpal ){
     vorpal.log( `Loaded ${ __filename }.` );
+
+    vorpal.command( 'handles' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Manage handles.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'handles add' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Bind EPUB handles.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'handles delete' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Unbind EPUB handles.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'handles delete all' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Unbind all EPUB handles.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
+
+    vorpal.command( 'handles full-replace' )
+        .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .description( 'Replace all EPUB handles.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
 };
