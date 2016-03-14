7999f773abfa9a6e4397eab96446276748675653
David <da70@nyu.edu>
2016-03-14 17:03:46 -0400

    verify: update commands to be able to work correctly and return responses when called by .execSync(..., {fatal: true})

--- commands/verify/index.js
+++ commands/verify/index.js
@@ -7,10 +7,18 @@ module.exports = function( vorpal ){
         .description( 'Verify integrity of published collection, handles, and metadata indexes.' )
         .action(
             function( args, callback ) {
+                let result = false;
+
                 vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
                 vorpal.log( args );
 
-                callback();
+                // If called via `.execSync`, `callback` will be undefined,
+                // and return values will be used as response.
+                if ( callback ) {
+                    callback();
+                } else {
+                    return result;
+                }
             }
         );
 };
