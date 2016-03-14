d6eed3ff00866986a2d610d4eff0a52c6baf4f3d
david <da70@nyu.edu>
2016-03-14 12:11:09 -0400

    verify: initial commit of shell

--- commands/verify/index.js
+++ commands/verify/index.js
@@ -2,4 +2,15 @@
 
 module.exports = function( vorpal ){
     vorpal.log( `Loaded ${ __filename }.` );
+
+    vorpal.command( 'verify' )
+        .description( 'Verify integrity of published collection, handles, and metadata indexes.' )
+        .action(
+            function( args, callback ) {
+                vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
+                vorpal.log( args );
+
+                callback();
+            }
+        );
 };
