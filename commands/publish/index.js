c59949b48735f342b9b633976a6315f042e0b702
David <da70@nyu.edu>
2016-03-14 16:36:29 -0400

    publish(subcommand): add { fatal : true } to each execSync()

--- commands/publish/index.js
+++ commands/publish/index.js
@@ -4,11 +4,11 @@ module.exports = function( vorpal ){
     vorpal.log( `Loaded ${ __filename }.` );
 
     function publish( subCommand ) {
-        vorpal.execSync( `content ${subCommand}` );
-        vorpal.execSync( `solr ${subCommand}` );
-        vorpal.execSync( `handles ${subCommand}` );
-        vorpal.execSync( `readium-json ${subCommand}` );
-        vorpal.execSync( `verify ${subCommand}` );
+        vorpal.execSync( `content ${subCommand}`,      { fatal : true } );
+        vorpal.execSync( `solr ${subCommand}`,         { fatal : true } );
+        vorpal.execSync( `handles ${subCommand}`,      { fatal : true } );
+        vorpal.execSync( `readium-json ${subCommand}`, { fatal : true } );
+        vorpal.execSync( `verify ${subCommand}`,       { fatal : true } );
     }
 
     vorpal.command( 'publish' )
