ccbeb9d15fb4604aff2528ed953847a5bf4e68fa
David <da70@nyu.edu>
2016-03-14 17:02:04 -0400

    content: update plain `content` command to be able to work correctly and return responses when called by .execSync(..., {fatal: true})

--- commands/content/index.js
+++ commands/content/index.js
@@ -8,10 +8,18 @@ module.exports = function( vorpal ){
         .description( 'Manage exploded EPUB content.' )
         .action(
             function( args, callback ) {
+                let result = false;
+
                 vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
                 vorpal.log( args );
 
-                callback();
+                // If called via `.execSync`, `callback` will be undefined,
+                // and return values will be used as response.
+                if ( callback ) {
+                    callback();
+                } else {
+                    return result;
+                }
             }
         );
 
