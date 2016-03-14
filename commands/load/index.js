f31a66dd5e9c8c4dfc254fd021d62052ef1d27ee
David <da70@nyu.edu>
2016-03-14 16:59:46 -0400

    load: update commands to be able to work correctly and return responses when called by .execSync(..., {fatal: true})

--- commands/load/index.js
+++ commands/load/index.js
@@ -7,10 +7,18 @@ module.exports = function( vorpal ){
         .description( 'Read in configuration file and load resources.' )
         .action(
             function( args, callback ) {
+                let result = false;
+
                 vorpal.log(  `\`${this.commandWrapper.command}\` run with args:`  );
                 vorpal.log( args );
 
-                callback();
+                // If called via `.execSync`, `callback` will be undefined,
+                // and return values will be used as response.
+                if ( callback ) {
+                    callback();
+                } else {
+                    return result;
+                }
             }
         );
 };
