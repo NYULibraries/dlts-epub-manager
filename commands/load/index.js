04e496c83a33e303f4b4d0aab20e7449e06b6905
David <da70@nyu.edu>
2016-05-03 17:44:34 -0400

    load: add config file basename autocompletion

--- commands/load/index.js
+++ commands/load/index.js
@@ -1,11 +1,17 @@
 "use strict";
 
+let fs   = require( 'fs' );
+let path = require( 'path' );
+
+const CONFIG_FILE_EXTENSION = '.json';
+
 module.exports = function( vorpal ){
     vorpal.log( `Loaded ${ __filename }.` );
 
     vorpal.command( 'load <configuration>' )
         .description( 'Read in configuration file and load resources.' )
         .option( '--dry-run', 'Print actions taken but do not execute them.' )
+        .autocomplete( getConfigFileBasenames( vorpal.em.configDir ) )
         .action(
             function( args, callback ) {
                 let result = false;
@@ -23,3 +29,13 @@ module.exports = function( vorpal ){
             }
         );
 };
+
+function getConfigFileBasenames( dir ) {
+    let filenames = fs.readdirSync( dir );
+
+    return filenames.map(
+        function( filename ) {
+            return path.basename( filename, CONFIG_FILE_EXTENSION );
+        }
+    );
+}
