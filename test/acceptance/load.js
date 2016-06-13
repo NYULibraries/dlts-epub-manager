673a14cfbffd8748e703a936d9a6e186b4341b20
David <da70@nyu.edu>
2016-06-13 17:26:00 -0400

    test/acceptance/load.js: initial commit with load command w/ local metadataDir test

--- test/acceptance/load.js
+++ test/acceptance/load.js
@@ -2,13 +2,22 @@
 
 let assert = require( 'chai' ).assert;
 let em     = require( '../../lib/bootstrap' );
+let stringify = require( 'json-stable-stringify' );
 let vorpal = em.vorpal;
 
 vorpal.em.configDir = __dirname + '/fixture/config';
 
-describe( 'load', () => {
-    it( 'should work', () => {
-        vorpal.parse( [ null, null, 'load', 'test-full-repo-load' ] );
+describe( 'load command', () => {
+    it( 'should correctly load from local metadataDir', () => {
+        let expected = stringify(
+            require( './fixture/metadata/full'),
+            { stable : '    ' }
+        );
+
+        vorpal.parse( [ null, null, 'load', 'test-full-filesystem-load' ] );
+        let actual = vorpal.em.metadata.dumpCanonical();
+
+        assert( actual === expected, 'Loaded metadata did not match expected.' );
     } );
 } );
 
