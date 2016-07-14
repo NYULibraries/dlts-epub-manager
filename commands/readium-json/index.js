19372b18c907d9533c3583ab4cc5f0f14d9e7787
David <da70@nyu.edu>
2016-07-14 12:30:52 -0400

    readium-json: remove --dry-run option

--- commands/readium-json/index.js
+++ commands/readium-json/index.js
@@ -4,7 +4,6 @@ module.exports = function( vorpal ){
     vorpal.log( `Loaded ${ __filename }.` );
 
     vorpal.command( 'readium-json' )
-        .option( '--dry-run', 'Print actions taken but do not execute them.' )
         .description( 'Manage `epub_library.json` file.' )
         .action(
             function( args, callback ) {
@@ -24,7 +23,6 @@ module.exports = function( vorpal ){
         );
 
     vorpal.command( 'readium-json add' )
-        .option( '--dry-run', 'Print actions taken but do not execute them.' )
         .description( 'Add EPUBs to `epub_library.json` file.' )
         .action(
             function( args, callback ) {
@@ -44,7 +42,6 @@ module.exports = function( vorpal ){
         );
 
     vorpal.command( 'readium-json delete' )
-        .option( '--dry-run', 'Print actions taken but do not execute them.' )
         .description( 'Delete EPUBs from `epub_library.json` file.' )
         .action(
             function( args, callback ) {
@@ -64,7 +61,6 @@ module.exports = function( vorpal ){
         );
 
     vorpal.command( 'readium-json delete all' )
-        .option( '--dry-run', 'Print actions taken but do not execute them.' )
         .description( 'Delete all EPUBs from `epub_library.json` file.' )
         .action(
             function( args, callback ) {
@@ -84,7 +80,6 @@ module.exports = function( vorpal ){
         );
 
     vorpal.command( 'readium-json full-replace' )
-        .option( '--dry-run', 'Print actions taken but do not execute them.' )
         .description( 'Replace entire `epub_library.json` file.' )
         .action(
             function( args, callback ) {
