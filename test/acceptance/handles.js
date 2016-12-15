d3dc83afbc7a56849ba83f7e185347aad0d086fe
David <da70@nyu.edu>
2016-12-15 13:45:37 -0500

    test/acceptance/handles.js: override vorpal.em.request with a stub

--- test/acceptance/handles.js
+++ test/acceptance/handles.js
@@ -5,11 +5,12 @@
 let assert    = require( 'chai' ).assert;
 let em        = require( '../../lib/bootstrap' );
 let _         = require( 'lodash' );
-let request   = require( 'sync-request' );
 let vorpal    = em.vorpal;
 
 const CONF               = 'full-metadataDir';
 const HANDLE_TEST_SERVER = 'handle';
+
+let overriddenRequest;
 
 vorpal.em.configDir = __dirname + '/fixture/config';
 
@@ -18,50 +19,62 @@ let conf;
 describe( 'handles command', () => {
 
     before( ( ) => {
+        overriddenRequest = vorpal.em.request;
+
+        vorpal.em.request = httpRequestStub();
+
         let loadSucceeded = loadConfiguration( CONF );
 
         assert( loadSucceeded === true,
                 'ERROR: beforeEach() is not set up right.  ' +
                 `Failed to load configuration "${CONF}".` );
-
-        // TODO: start up mock handle server and assert that it is up and running.
     } );
 
     it( 'should correctly add all handles to handle server', () => {
         vorpal.parse( [ null, null, 'handles', 'add', 'full-metadataDir' ] );
     } );
 
+    it( 'should correctly delete all handles from handles server', () => {
+
+        vorpal.parse( [ null, null, 'handles', 'delete', 'all' ] );
+
+    } );
+
+    it( 'should correctly delete 3 handles from handles server', () => {
+
+        vorpal.parse( [ null, null, 'handles', 'delete', 'delete-3' ] );
+
+    } );
+
+    it( 'should correctly add all handles to handles server', () => {
+
+        vorpal.parse( [ null, null, 'handles', 'add', 'full-metadataDir' ] );
+
+    } );
+
+    it( 'should correctly add 3 replacement handles and 3 new handles to handles server', () => {
+
+        vorpal.parse( [ null, null, 'handles', 'add', 'replace-3-new-3' ] );
+
+    } );
+
+    it( 'should correctly full-replace full handles with replace-3-add-3 handles in handles server', () => {
+
+        vorpal.parse( [ null, null, 'handles', 'full-replace', 'replace-3-new-3' ] );
+
+    } );
+
+    after( ( ) => {
+        vorpal.em.request = overriddenRequest;
+    } );
+
 } );
 
-it( 'should correctly delete all handles from handles server', () => {
-
-    vorpal.parse( [ null, null, 'handles', 'delete', 'all' ] );
-
-} );
-
-it( 'should correctly delete 3 handles from handles server', () => {
-
-    vorpal.parse( [ null, null, 'handles', 'delete', 'delete-3' ] );
-
-} );
-
-it( 'should correctly add all handles to handles server', () => {
-
-    vorpal.parse( [ null, null, 'handles', 'add', 'full-metadataDir' ] );
-
-} );
-
-it( 'should correctly add 3 replacement handles and 3 new handles to handles server', () => {
-
-    vorpal.parse( [ null, null, 'handles', 'add', 'replace-3-new-3' ] );
-
-} );
-
-it( 'should correctly full-replace full handles with replace-3-add-3 handles in handles server', () => {
-
-    vorpal.parse( [ null, null, 'handles', 'full-replace', 'replace-3-new-3' ] );
-
-} );
+function httpRequestStub() {
+    return function( method, url, options ) {
+        console.log( `${method} ${url} with options ${options}!` );
+    }
+}
 
 function loadConfiguration( confName ) {
     let loadSucceeded = vorpal.execSync( `load ${confName}`, { fatal : true } );
