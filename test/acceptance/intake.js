6dd7089454c85b57f377cbb1dd293e0a4e66887f
David <da70@nyu.edu>
2017-04-15 20:44:25 -0400

    intake tests: verify that conf file directories are pointing to correct tmp/ directories; clean tmp/{epubs,metadata}/ at beginning of tests

--- test/acceptance/intake.js
+++ test/acceptance/intake.js
@@ -5,9 +5,13 @@
 let assert     = require( 'chai' ).assert;
 let dircompare = require( 'dir-compare' );
 let em         = require( '../../lib/bootstrap' );
+let path       = require( 'path' );
+let rimraf     = require( 'rimraf' );
 let vorpal     = em.vorpal;
 
 const CONF = 'intake';
+const TMP_EPUBS    = __dirname + '/tmp/epubs';
+const TMP_METADATA = __dirname + '/tmp/metadata';
 
 vorpal.em.configDir        = __dirname + '/fixture/config';
 vorpal.em.configPrivateDir = __dirname + '/fixture/config-private';
@@ -20,14 +24,25 @@ describe( 'intake command', () => {
         assert( loadSucceeded === true,
                 'ERROR: before() is not set up right.  ' +
                 `Failed to load configuration "${CONF}".` );
-    } );
 
-    beforeEach( ( ) => {
+        assert(
+            // Conf file epubOutputDir is relative path, have to change it to
+            // absolute for comparison
+            path.dirname( path.dirname ( __dirname ) ) + '/' +
+                vorpal.em.conf.epubOutputDir === TMP_EPUBS,
+            `epubOutputDir is ${TMP_EPUBS}`
+        );
+
+        assert(
+            // Conf file metadataDir is relative path, have to change it to
+            // absolute for comparison
+            path.dirname( path.dirname ( __dirname ) ) + '/' +
+                vorpal.em.conf.metadataDir === TMP_METADATA,
+            `metadataDir is ${TMP_METADATA}`
+        );
     } );
 
     it( 'should correctly intake all EPUBs and generate correct Readium versions', () => {
-        vorpal.parse( [ null, null, 'intake', 'add' ] );
-
         var epubsComparison,
             epubOutputDir = vorpal.em.conf.epubOutputDir,
             epubExpectedDir = __dirname + '/expected/epubs-from-intake',
@@ -35,6 +50,16 @@ describe( 'intake command', () => {
                 compareContent : true,
                 excludeFilter  : '.commit-empty-directory',
             };
+
+        try {
+            rimraf.sync( epubOutputDir + '/*' );
+        } catch ( error ) {
+            vorpal.log( `ERROR clearing ${epubOutputDir}: ${error}` );
+
+            process.exit(1);
+        }
+
+        vorpal.parse( [ null, null, 'intake', 'add' ] );
 
         epubsComparison = dircompare.compareSync(
             epubOutputDir, epubExpectedDir,
@@ -45,8 +70,6 @@ describe( 'intake command', () => {
     } );
 
     it( 'should correctly intake all EPUBs and generate correct metadata files', () => {
-        vorpal.parse( [ null, null, 'intake', 'add' ] );
-
         var metadataComparison,
             metadataDir = vorpal.em.conf.metadataDir,
             metadataExpectedDir = __dirname + '/expected/metadata-from-intake',
@@ -54,6 +77,16 @@ describe( 'intake command', () => {
                 compareContent : true,
                 excludeFilter  : '.commit-empty-directory',
             };
+
+        try {
+            rimraf.sync( metadataDir + '/*' );
+        } catch ( error ) {
+            vorpal.log( `ERROR clearing ${metadataDir}: ${error}` );
+
+            process.exit(1);
+        }
+
+        vorpal.parse( [ null, null, 'intake', 'add' ] );
 
         metadataComparison = dircompare.compareSync(
             metadataDir, metadataExpectedDir,
