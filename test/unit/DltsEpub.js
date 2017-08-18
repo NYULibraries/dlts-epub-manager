3ce5114d3b205ab1da8e0995be7e4e24af4fec48
David <da70@nyu.edu>
2017-08-18 18:04:27 -0400

    DltsEpub test: use new expected/ JSON files for testing

--- test/unit/DltsEpub.js
+++ test/unit/DltsEpub.js
@@ -1,71 +1,73 @@
 "use strict";
 
-let assert   = require( 'chai' ).assert,
+/* global before, beforeEach */
+
+let fs = require( 'fs' ),
+
+    assert   = require( 'chai' ).assert,
     _        = require( 'lodash' ),
 
     DltsEpub = require( '../../lib/epub/DltsEpub' ).DltsEpub;
 
+const EXPECTED_DIR       = __dirname + '/expected';
+const EXPECTED_DATA_FILE = 'expected-data.json';
+
+const FIXTURE_DIR = __dirname + '/fixture';
+
 describe( 'epub', () => {
-    const TEST_EXPLODED_EPUB_DIR = __dirname + '/fixture/9780814780978';
+    let expectedData = {};
 
-    const EXPECTED_PACKAGE = {
-        creators           : [
-            'Ian Shapiro',
-            'Robert Adams',
-        ],
-        date               : '1998',
-        format             : '351 Pages',
-        identifiers        : [ '9780814780978' ],
-        language           : 'En',
-        publisher          : 'New York University Press',
-        rights             : 'All rights reserved.',
-        title              : 'Integrity and Conscience',
-        uniqueIdentifier   : '9780814780978',
-    };
-    Object.freeze( EXPECTED_PACKAGE );
+    before( () => {
+        let epubId = fs.readdirSync( EXPECTED_DIR );
 
-    const EXPECTED_DLTS_METADATA = {
-        identifier : '9780814780978'
-    };
-    Object.freeze( EXPECTED_DLTS_METADATA );
+        epubId.forEach( ( epubId ) => {
+            expectedData[ epubId ] =
+                require( `${EXPECTED_DIR}/${epubId}/${EXPECTED_DATA_FILE}` );
+        } );
+    } );
 
     // Generally would like to stick to the principle of 1 assert per test, but
     // for now will try this construction, which is quite simple, succinct, and
     // easy to understand, and which makes changes to testing of Epub
     // fields quiet easy.
     it( 'should construct a DltsEpub object with correct package fields', () => {
-        let dltsEpub = new DltsEpub( TEST_EXPLODED_EPUB_DIR );
-        let field;
+        Object.keys( expectedData ).forEach( ( epubId ) => {
+            let dltsEpub        = new DltsEpub( `${FIXTURE_DIR}/${epubId}` );
+            let expectedPackage = expectedData[ epubId ].package;
+            let field;
 
-        for ( field in EXPECTED_PACKAGE ) {
-            if ( EXPECTED_PACKAGE.hasOwnProperty( field ) ) {
-                let got      = dltsEpub.package[ field ];
-                let expected = EXPECTED_PACKAGE[ field ];
+            for ( field in expectedPackage ) {
+                if ( expectedPackage.hasOwnProperty( field ) ) {
+                    let got      = dltsEpub.package[ field ];
+                    let expected = expectedPackage[ field ];
 
-                assert(
-                    _.isEqual( got, expected ) === true,
-                    `Incorrect field "${field}": got "${got}" but was expecting "${expected}"`
-                );
+                    assert(
+                        _.isEqual( got, expected ) === true,
+                        `${epubId}: got field "${field}" value of "${got}" but was expecting "${expected}"`
+                    );
+                }
             }
-        }
-
+        } );
     } );
 
     it( 'should construct a DltsEpub object with correct dlts fields', () => {
-        let dltsEpub = new DltsEpub( TEST_EXPLODED_EPUB_DIR );
-        let field;
+        Object.keys( expectedData ).forEach( ( epubId ) => {
 
-        for ( field in EXPECTED_DLTS_METADATA ) {
-            if ( EXPECTED_DLTS_METADATA.hasOwnProperty( field ) ) {
-                let got      = dltsEpub.dlts.metadata[ field ];
-                let expected = EXPECTED_DLTS_METADATA[ field ];
+            let dltsEpub             = new DltsEpub( `${FIXTURE_DIR}/${epubId}` );
+            let expectedDltsMetadata = expectedData[ epubId ].dlts.metadata;
+            let field;
 
-                assert(
-                    _.isEqual( got, expected ) === true,
-                    `Incorrect field "${field}": got "${got}" but was expecting "${expected}"`
-                );
+            for ( field in expectedDltsMetadata ) {
+                if ( expectedDltsMetadata.hasOwnProperty( field ) ) {
+                    let got      = dltsEpub.dlts.metadata[ field ];
+                    let expected = expectedDltsMetadata[ field ];
+
+                    assert(
+                        _.isEqual( got, expected ) === true,
+                        `${epubId}: got field "${field}" value of ${got} but was expecting "${expected}"`
+                    );
+                }
             }
-        }
-
+        } );
     } );
 } );
