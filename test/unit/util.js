c510a90c4dae3a06d7e53fbf6cd832ab76ab2fd6
David <da70@nyu.edu>
2017-08-19 17:59:14 -0400

    test/unit/util.js: re-order const declarations

--- test/unit/util.js
+++ test/unit/util.js
@@ -5,75 +5,6 @@ let assert = require( 'chai' ).assert,
     util   = require( '../../lib/util/index' );
 
 describe( 'util', () => {
-    // From https://www.safaribooksonline.com/library/view/regular-expressions-cookbook/9781449327453/ch04s13.html
-    const VALID_ISBN_13_STRINGS = [
-        'ISBN 978-0-596-52068-7',
-        'ISBN-13: 978-0-596-52068-7',
-        '978 0 596 52068 7',
-        '9780596520687',
-
-        'ISBN 979-0-596-52068-7',
-        'ISBN-13: 979-0-596-52068-7',
-        '979 0 596 52068 7',
-        '9790596520687'
-    ];
-
-    const VALID_ISBN_10_STRINGS = [
-        'ISBN-10 0-596-52068-9',
-        '0-596-52068-9'
-    ];
-
-    const INVALID_ISBN_STRINGS = [
-        // 3rd digit is invalid
-        'ISBN 976-0-596-52068-7',
-        'ISBN-13: 976-0-596-52068-7',
-        '976 0 596 52068 7',
-        '9760596520687',
-
-        // Incorrect separation
-        'ISBN 9780-596-52068-7',
-        'ISBN-13: 9780-596-52068-7',
-        '9780 596 52068 7',
-        '978 0  596 52068 7',
-        '978-0596520687',
-
-        // Leading and trailing whitespace
-        ' 978 0 596 52068 7',
-        '978 0 596 52068 7 ',
-
-        'total garbage'
-    ];
-
-    const VALID_NORMALIZED_ISBN_13_STRINGS= [
-        '9790814793114',
-        '9790814793398',
-        '9791479824243',
-        '9791479899982',
-
-        '9780814793114',
-        '9780814793398',
-        '9781479824243',
-        '9781479899982'
-    ];
-
-    const INVALID_NORMALIZED_ISBN_13_STRINGS = [
-        // Not normalized
-        'ISBN 978-0-596-52068-7',
-        'ISBN-13: 978-0-596-52068-7',
-        '978 0 596 52068 7',
-
-        // 3rd digit is invalid
-        '9760814793114',
-        '9760814793398',
-        '9761479824243',
-        '9761479899982',
-
-        // Normalized, but ISBN-10
-        '0596520689',
-
-        ...INVALID_ISBN_STRINGS
-    ];
-
     // Some test authors to add from U. Michigan and U. Minnesota presses, when
     // ready to make the necessary improvments to util.getAuthorSortKey:
     //
@@ -239,6 +170,75 @@ describe( 'util', () => {
         "Transformation of Rage" : "Transformation of Rage",
         "The Truth About Freud's Technique" : "Truth About Freud's Technique",
     };
+
+    // From https://www.safaribooksonline.com/library/view/regular-expressions-cookbook/9781449327453/ch04s13.html
+    const VALID_ISBN_13_STRINGS = [
+        'ISBN 978-0-596-52068-7',
+        'ISBN-13: 978-0-596-52068-7',
+        '978 0 596 52068 7',
+        '9780596520687',
+
+        'ISBN 979-0-596-52068-7',
+        'ISBN-13: 979-0-596-52068-7',
+        '979 0 596 52068 7',
+        '9790596520687'
+    ];
+
+    const VALID_ISBN_10_STRINGS = [
+        'ISBN-10 0-596-52068-9',
+        '0-596-52068-9'
+    ];
+
+    const INVALID_ISBN_STRINGS = [
+        // 3rd digit is invalid
+        'ISBN 976-0-596-52068-7',
+        'ISBN-13: 976-0-596-52068-7',
+        '976 0 596 52068 7',
+        '9760596520687',
+
+        // Incorrect separation
+        'ISBN 9780-596-52068-7',
+        'ISBN-13: 9780-596-52068-7',
+        '9780 596 52068 7',
+        '978 0  596 52068 7',
+        '978-0596520687',
+
+        // Leading and trailing whitespace
+        ' 978 0 596 52068 7',
+        '978 0 596 52068 7 ',
+
+        'total garbage'
+    ];
+
+    const VALID_NORMALIZED_ISBN_13_STRINGS= [
+        '9790814793114',
+        '9790814793398',
+        '9791479824243',
+        '9791479899982',
+
+        '9780814793114',
+        '9780814793398',
+        '9781479824243',
+        '9781479899982'
+    ];
+
+    const INVALID_NORMALIZED_ISBN_13_STRINGS = [
+        // Not normalized
+        'ISBN 978-0-596-52068-7',
+        'ISBN-13: 978-0-596-52068-7',
+        '978 0 596 52068 7',
+
+        // 3rd digit is invalid
+        '9760814793114',
+        '9760814793398',
+        '9761479824243',
+        '9761479899982',
+
+        // Normalized, but ISBN-10
+        '0596520689',
+
+        ...INVALID_ISBN_STRINGS
+    ];
 
     describe( '#getAuthorSortKey', () => {
         it( 'should return correct author sort keys', () => {
