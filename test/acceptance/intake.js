5d287211cadb9034468d1b44148b598fd4d56e83
David <da70@nyu.edu>
2018-11-02 00:04:11 -0400

    intake tests: initial commit of --skip-metadata test -- NOT WORKING YET

--- test/acceptance/intake.js
+++ test/acceptance/intake.js
@@ -12,6 +12,7 @@ let rimraf     = require( 'rimraf' );
 let vorpal     = em.vorpal;
 
 const CONF_INTAKE_FULL = 'intake-full';
+const CONF_INTAKE_1    = 'intake-1';
 const TMP_EPUBS        = __dirname + '/tmp/epubs';
 const TMP_METADATA     = __dirname + '/tmp/metadata';
 
@@ -111,4 +112,84 @@ describe( 'intake command', function() {
         assert( metadataComparison.same === true, `${metadataDir} does not match ${metadataExpectedDir}` );
     } );
 
+    it( 'should correctly intake an EPUB with --skip-metadata and generate correct Readium version but not generate metadata files', function() {
+        let loadSucceeded = vorpal.execSync( `load ${CONF_INTAKE_1}`, { fatal : true } );
+
+        assert( loadSucceeded === true,
+                'ERROR: test is not set up right.  ' +
+                `Failed to load configuration "${CONF_INTAKE_1}".` );
+
+        assert(
+            // Conf file epubOutputDir is relative path, have to change it to
+            // absolute for comparison
+            path.dirname( path.dirname ( __dirname ) ) + '/' +
+            vorpal.em.conf.intakeOutputDir === TMP_EPUBS,
+            `intakeOutputDir is not ${TMP_EPUBS}`
+        );
+
+        assert(
+            // Conf file metadataDir is relative path, have to change it to
+            // absolute for comparison
+            path.dirname( path.dirname ( __dirname ) ) + '/' +
+            vorpal.em.conf.metadataDir === TMP_METADATA,
+            `metadataDir is not ${TMP_METADATA}`
+        );
+
+        let epubsComparison,
+
+            intakeOutputDir   = vorpal.em.conf.intakeOutputDir,
+            intakeExpectedDir = __dirname + '/expected/epubs-from-intake',
+
+            metadataDir = vorpal.em.conf.metadataDir,
+
+            // For now, just comparing thumbnail filenames since binary diffs will always fail.
+            thumbnailsExpected = glob.sync( '**/*-th.jpg', {cwd : intakeExpectedDir} ),
+            thumbnailsGot,
+
+            compareOptions = {
+                compareContent : true,
+                // Excluding *-th.jpg because the thumbnails look identical, but
+                // differ on a binary level.
+                excludeFilter  : '.commit-empty-directory,*-th.jpg',
+            };
+
+        try {
+            rimraf.sync( intakeOutputDir + '/*' );
+        } catch ( error ) {
+            vorpal.log( `ERROR clearing ${intakeOutputDir}: ${error}` );
+
+            process.exit(1);
+        }
+
+        try {
+            rimraf.sync( TMP_METADATA + '/*' );
+        } catch ( error ) {
+            vorpal.log( `ERROR clearing ${TMP_METADATA}: ${error}` );
+
+            process.exit(1);
+        }
+
+        vorpal.parse( [ null, null, 'intake', 'add', '--skip-metadata' ] );
+
+        // Normally would like to keep to a single assert per test, but making an
+        // exception here because the test intake is such an expensive operation,
+        // would like to avoid repeating it unecessarily.
+
+        thumbnailsGot = glob.sync( '**/*-th.jpg', {cwd : intakeOutputDir} );
+        assert(
+            _.isEqual( thumbnailsGot, thumbnailsExpected ),
+            'Not all cover image thumbnails were created'
+        );
+
+        epubsComparison = dircompare.compareSync(
+            intakeOutputDir, intakeExpectedDir,
+            compareOptions
+        );
+
+        assert( epubsComparison.same === true, `${intakeOutputDir} matched ${intakeExpectedDir}` );
+
+        // TODO
+        metadataDir = false;
+        assert( metadataDirIsEmpty, `${metadataDir} is not empty` );
+    } );
 } );
