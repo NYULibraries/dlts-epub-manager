d75e2e9a44dafaa3d19e14b055bd01455a92e2df
David <da70@nyu.edu>
2018-11-01 23:54:18 -0400

    intake test: move before() code to test

--- test/acceptance/intake.js
+++ test/acceptance/intake.js
@@ -25,18 +25,21 @@ describe( 'intake command', function() {
     // Avoid "Error: timeout of 2000ms exceeded. Ensure the done() callback is being called in this test."
     this.timeout( 60000 );
 
-    before( ( ) => {
+    it( 'should correctly intake all EPUBs and generate correct Readium versions and metadata files', function() {
+        // Normally would like to keep to a single assert per test, but making an
+        // exception here because the test intake is such an expensive operation,
+        // would like to avoid repeating it unecessarily.
         let loadSucceeded = vorpal.execSync( `load ${CONF}`, { fatal : true } );
 
         assert( loadSucceeded === true,
-                'ERROR: before() is not set up right.  ' +
+                'ERROR: test is not set up right.  ' +
                 `Failed to load configuration "${CONF}".` );
 
         assert(
             // Conf file epubOutputDir is relative path, have to change it to
             // absolute for comparison
             path.dirname( path.dirname ( __dirname ) ) + '/' +
-                vorpal.em.conf.intakeOutputDir === TMP_EPUBS,
+            vorpal.em.conf.intakeOutputDir === TMP_EPUBS,
             `intakeOutputDir is not ${TMP_EPUBS}`
         );
 
@@ -44,15 +47,9 @@ describe( 'intake command', function() {
             // Conf file metadataDir is relative path, have to change it to
             // absolute for comparison
             path.dirname( path.dirname ( __dirname ) ) + '/' +
-                vorpal.em.conf.metadataDir === TMP_METADATA,
+            vorpal.em.conf.metadataDir === TMP_METADATA,
             `metadataDir is not ${TMP_METADATA}`
         );
-    } );
-
-    it( 'should correctly intake all EPUBs and generate correct Readium versions and metadata files', function() {
-        // Normally would like to keep to a single assert per test, but making an
-        // exception here because the test intake is such an expensive operation,
-        // would like to avoid repeating it unecessarily.
 
         let epubsComparison,
 
