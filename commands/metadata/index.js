f3f50974451903940b292b9995b9ef2eba1ae6df
David <da70@nyu.edu>
2020-12-12 12:51:03 -0500

    metadata command: remove unused execSync require

--- commands/metadata/index.js
+++ commands/metadata/index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const execSync  = require( 'child_process' ).execSync;
 const fs        = require( 'fs' );
 const path      = require( 'path' );
 const rimraf    = require( 'rimraf' );
